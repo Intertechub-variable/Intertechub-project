a2671d5955fb7060e621ed1f67b7d8fdc39b44b9
Yonatan Tizazu <yonatantizazu12@gmail.com>
2025-01-08 10:17:16 +0300

    feat: implement signup and logout

--- server-side/controllers/login.js
+++ server-side/controllers/login.js
@@ -6,13 +6,11 @@ const login = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    // Check if user exists
     const user = await User.findOne({ email });
     if (!user) {
       return res.status(400).json({ message: 'Invalid credential' });
     }
 
-    // Check if password is correct
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(400).json({ message: 'Invalid credential' });
@@ -21,7 +19,8 @@ const login = async (req, res) => {
     // Create and sign a token
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
-    res.json({ token, user: { id: user._id, email: user.email } });
+    res.cookie('token', token, { httpOnly: true });
+    res.json({ user: { id: user._id, email: user.email } });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: 'Server error' });
